06eb34c5e42c89f2e35c4feba5acae7d277c880e
ashutoshmallik610 <157103768+ashutoshmallik610@users.noreply.github.com>
2026-08-08 17:37:20 +0530

    teacher

--- src/com/smartcity/Teacher.java
+++ src/com/smartcity/Teacher.java
@@ -2,7 +2,9 @@ package com.smartcity;
 
 public class Teacher extends Employee
 {
-
+	private String subject;
+	private String schoolName;
+	
 	public Teacher(String name, int age, 
 			String gender, String address, String phone, String empId,
 			String dept, int salary) 
@@ -19,7 +21,18 @@ public class Teacher extends Employee
 	{
 		System.out.println("Teachers conducts exam");
 	}
+	public void updateSubject(String newSubject)
+	{
+	    this.subject = newSubject;
+	    System.out.println("Subject updated successfully.");
+	}
 
+	public void updateSchool(String newSchool)
+	{
+	    this.schoolName = newSchool;
+	    System.out.println("School updated successfully.");
+	}
+	
 	@Override
 	public void generateReport() {
 		
